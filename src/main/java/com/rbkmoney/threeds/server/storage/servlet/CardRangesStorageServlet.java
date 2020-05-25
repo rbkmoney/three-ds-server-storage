fa37fe0e30c62fd7f70730ae0856e00fabeef355
a.romanov <a.romanov@rbkmoney.com>
2020-05-25 14:38:21 +0300

    BJ-917: Add servlets

---
+++ src/main/java/com/rbkmoney/threeds/server/storage/servlet/CardRangesStorageServlet.java
@@ -0,0 +1,34 @@
+package com.rbkmoney.threeds.server.storage.servlet;
+
+import com.rbkmoney.damsel.three_ds_server_storage.CardRangesStorageSrv;
+import com.rbkmoney.threeds.server.storage.handler.CardRangesStorageHandler;
+import com.rbkmoney.woody.thrift.impl.http.THServiceBuilder;
+import lombok.RequiredArgsConstructor;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@WebServlet("/three-ds-server-storage/card-ranges")
+public class CardRangesStorageServlet extends GenericServlet {
+
+    private final CardRangesStorageHandler handler;
+
+    private Servlet servlet;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        servlet = new THServiceBuilder()
+                .build(CardRangesStorageSrv.Iface.class, handler);
+    }
+
+    @Override
+    public void service(
+            ServletRequest request,
+            ServletResponse response) throws ServletException, IOException {
+        servlet.service(request, response);
+    }
+}
+
