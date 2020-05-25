1446d93f715880c59a4b5891d0c71d8f04845a5a
a.romanov <a.romanov@rbkmoney.com>
2020-05-25 17:17:41 +0300

    BJ-917: Implement handlers

--- src/main/java/com/rbkmoney/threeds/server/storage/handler/RReqTransactionInfoStorageHandler.java
+++ src/main/java/com/rbkmoney/threeds/server/storage/handler/RReqTransactionInfoStorageHandler.java
@@ -2,17 +2,41 @@ package com.rbkmoney.threeds.server.storage.handler;
 
 import com.rbkmoney.damsel.three_ds_server_storage.RReqTransactionInfo;
 import com.rbkmoney.damsel.three_ds_server_storage.RReqTransactionInfoStorageSrv;
-import org.apache.thrift.TException;
+import com.rbkmoney.threeds.server.storage.entity.RReqTransactionInfoEntity;
+import com.rbkmoney.threeds.server.storage.repository.RReqTransactionInfoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.sql.Date;
+
+@Service
+@RequiredArgsConstructor
 public class RReqTransactionInfoStorageHandler implements RReqTransactionInfoStorageSrv.Iface {
 
+    private final RReqTransactionInfoRepository repository;
+
+    // TODO [a.romanov]: mappers
+
     @Override
-    public void saveRReqTransactionInfo(RReqTransactionInfo rReqTransactionInfo) throws TException {
-        throw new UnsupportedOperationException("TODO");
+    public void saveRReqTransactionInfo(RReqTransactionInfo transactionInfo) {
+        RReqTransactionInfoEntity entity = RReqTransactionInfoEntity.builder()
+                .id(transactionInfo.getTransactionId())
+                .acsDecConInd(transactionInfo.getAcsDecConInd())
+                .deviceChannel(transactionInfo.getDeviceChannel())
+                .decoupledAuthMaxTime(Date.valueOf(transactionInfo.getDecoupledAuthMaxTime()))
+                .build();
+
+        repository.save(entity);
     }
 
     @Override
-    public RReqTransactionInfo getRReqTransactionInfo(String s) throws TException {
-        throw new UnsupportedOperationException("TODO");
+    public RReqTransactionInfo getRReqTransactionInfo(String transactionId) {
+        RReqTransactionInfoEntity entity = repository.getOne(transactionId);
+
+        return new RReqTransactionInfo()
+                .setTransactionId(transactionId)
+                .setAcsDecConInd(entity.getAcsDecConInd())
+                .setDeviceChannel(entity.getDeviceChannel())
+                .setDecoupledAuthMaxTime(entity.getDecoupledAuthMaxTime().toString());
     }
 }
