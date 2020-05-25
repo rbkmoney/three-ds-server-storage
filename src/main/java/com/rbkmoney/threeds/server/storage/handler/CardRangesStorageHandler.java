72652f6e7d9255fd4a76f830aa68a839fe3978c0
a.romanov <a.romanov@rbkmoney.com>
2020-05-25 17:17:41 +0300

    BJ-917: Implement handlers

--- src/main/java/com/rbkmoney/threeds/server/storage/handler/CardRangesStorageHandler.java
+++ src/main/java/com/rbkmoney/threeds/server/storage/handler/CardRangesStorageHandler.java
@@ -1,12 +1,23 @@
 package com.rbkmoney.threeds.server.storage.handler;
 
-import com.rbkmoney.damsel.three_ds_server_storage.CardRangesStorageSrv;
-import com.rbkmoney.damsel.three_ds_server_storage.GetCardRangesRequest;
-import com.rbkmoney.damsel.three_ds_server_storage.GetCardRangesResponse;
-import com.rbkmoney.damsel.three_ds_server_storage.InitRBKMoneyPreparationFlowRequest;
+import com.rbkmoney.damsel.three_ds_server_storage.*;
+import com.rbkmoney.threeds.server.storage.entity.CardRangeEntity;
+import com.rbkmoney.threeds.server.storage.repository.CardRangesRepository;
+import lombok.RequiredArgsConstructor;
 import org.apache.thrift.TException;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+@RequiredArgsConstructor
 public class CardRangesStorageHandler implements CardRangesStorageSrv.Iface {
+
+    private final CardRangesRepository repository;
+
+    // TODO [a.romanov]: mappers
 
     @Override
     public void initRBKMoneyPreparationFlow(InitRBKMoneyPreparationFlowRequest initRBKMoneyPreparationFlowRequest) throws TException {
@@ -14,7 +25,19 @@ public class CardRangesStorageHandler implements CardRangesStorageSrv.Iface {
     }
 
     @Override
-    public GetCardRangesResponse getCardRanges(GetCardRangesRequest getCardRangesRequest) throws TException {
-        throw new UnsupportedOperationException("TODO");
+    public GetCardRangesResponse getCardRanges(GetCardRangesRequest getCardRangesRequest) {
+        String providerId = getCardRangesRequest.getProviderId();
+        List<CardRangeEntity> entities = repository.findByProviderId(providerId);
+
+        List<CardRange> cardRanges = entities.stream()
+                .map(e -> new CardRange()
+                        .setRangeStart(e.getRangeStart())
+                        .setRangeEnd(e.getRangeEnd()))
+                .collect(toList());
+
+        // TODO [a.romanov]: lastUpdatedAt?
+        return new GetCardRangesResponse()
+                .setProviderId(providerId)
+                .setCardRanges(cardRanges);
     }
 }
