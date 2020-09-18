ce4d3e71b91e3dfbf6b63d113f1e82743a7fd44a
a.romanov <a.romanov@rbkmoney.com>
2020-09-18 12:38:23 +0300

    BJ-988: Add PreparationFlowServiceHandler

---
+++ src/main/java/com/rbkmoney/threeds/server/storage/exception/DeserializationException.java
@@ -0,0 +1,8 @@
+package com.rbkmoney.threeds.server.storage.exception;
+
+public class DeserializationException extends RuntimeException {
+
+    public DeserializationException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/com/rbkmoney/threeds/server/storage/handler/PreparationFlowServiceHandler.java
@@ -0,0 +1,21 @@
+package com.rbkmoney.threeds.server.storage.handler;
+
+import com.rbkmoney.damsel.three_ds_server_storage.InitRBKMoneyPreparationFlowRequest;
+import com.rbkmoney.threeds.server.storage.service.PreparationFlowService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PreparationFlowServiceHandler implements PreparationFlowServiceSrv.Iface {
+
+    private final PreparationFlowService preparationFlowService;
+
+    @Override
+    public void initRBKMoneyPreparationFlow(InitRBKMoneyPreparationFlowRequest request) {
+        log.info("Init RBK.money preparation flow for providerId={}", request.getProviderId());
+        preparationFlowService.init(request.getProviderId(), request.getMessageVersion());
+    }
+}
