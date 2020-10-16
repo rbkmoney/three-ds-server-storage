3f4dc74d06ce7d24d2ebdcf2cafb87561e7c87b4
karleowne <a.karlov@rbkmoney.com>
2020-10-16 15:24:00 +0700

    BJ-997: fix resttemplate

--- src/main/java/com/rbkmoney/threeds/server/storage/config/RestTemplateConfig.java
+++ src/main/java/com/rbkmoney/threeds/server/storage/config/RestTemplateConfig.java
@@ -1,25 +1,22 @@
 package com.rbkmoney.threeds.server.storage.config;
 
-import com.rbkmoney.woody.api.trace.ContextUtils;
-import com.rbkmoney.woody.api.trace.context.TraceContext;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.metrics.web.client.MetricsRestTemplateCustomizer;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.annotation.RequestScope;
 
 import javax.net.ssl.SSLContext;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
 import java.time.Duration;
 
 @Configuration
@@ -27,50 +24,35 @@ import java.time.Duration;
 public class RestTemplateConfig {
 
     @Bean
-    public SSLContext sslContext() throws NoSuchAlgorithmException, KeyManagementException {
-        return new SSLContextBuilder().build();
+    @RequestScope
+    public RestTemplate restTemplate(
+            MetricsRestTemplateCustomizer metricsRestTemplateCustomizer,
+            @Value("${client.three-ds-server.timeout}") int networkTimeout) {
+        RestTemplate restTemplate = new org.springframework.boot.web.client.RestTemplateBuilder()
+                .requestFactory(() -> new HttpComponentsClientHttpRequestFactory(httpClient()))
+                .additionalCustomizers(metricsRestTemplateCustomizer)
+                .setConnectTimeout(Duration.ofMillis(networkTimeout))
+                .setReadTimeout(Duration.ofMillis(networkTimeout))
+                .build();
+        setMessageConverter(restTemplate);
+        return restTemplate;
     }
 
-    @Bean
-    public CloseableHttpClient httpClient(SSLContext sslContext) {
+    private CloseableHttpClient httpClient() {
+        SSLContext sslContext = sslContext();
+
         return HttpClients.custom()
-                .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
-                .setSSLContext(sslContext)
+                .setSSLSocketFactory(new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE))
                 .disableAutomaticRetries()
                 .build();
     }
 
-    @Bean
-    public HttpComponentsClientHttpRequestFactory requestFactory(CloseableHttpClient httpClient) {
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-        requestFactory.setHttpClient(httpClient);
-        return requestFactory;
-    }
-
-    @Bean
-    public RestTemplateBuilder restTemplateBuilder(
-            HttpComponentsClientHttpRequestFactory requestFactory,
-            MetricsRestTemplateCustomizer metricsRestTemplateCustomizer) {
-        return new RestTemplateBuilder()
-                .requestFactory(() -> requestFactory)
-                .additionalCustomizers(metricsRestTemplateCustomizer);
-    }
-
-    @Bean
-    public RestTemplate restTemplate(
-            RestTemplateBuilder restTemplateBuilder,
-            @Value("${client.three-ds-server.timeout}") int networkTimeout) {
-        int executionTimeout = ContextUtils.getExecutionTimeout(
-                TraceContext.getCurrentTraceData().getServiceSpan(),
-                networkTimeout);
-
-        RestTemplate restTemplate = restTemplateBuilder
-                .setConnectTimeout(Duration.ofMillis(executionTimeout))
-                .setReadTimeout(Duration.ofMillis(executionTimeout))
-                .build();
-
-        setMessageConverter(restTemplate);
-        return restTemplate;
+    private SSLContext sslContext() {
+        try {
+            return new SSLContextBuilder().build();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
     private void setMessageConverter(RestTemplate restTemplate) {
